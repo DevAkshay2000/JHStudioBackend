0280ddc205b4c3c76cd6aa40ec9658733caeb0d0
Akshay.s <a.sutar@saniconservices.com>
2024-10-03 15:26:43 +0530

    Port changes to port 1

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import cors from "cors"; // Enable Cross-Origin Resource Sharing
 import itemsRoute from "./routes/item.route";
 import itemDescription from "./routes/item-description.route";
 import itemImage from "./routes/item-images.route";
-
+import dotenv from 'dotenv'
+import path from 'path'
+// Load environment variables from .env file
+dotenv.config({ path: path.join(__dirname, '.env') });
 //**** import routes end
 //**** import middleware start
 // import { appDataSource } from "./config/dbconfig";
@@ -32,28 +35,35 @@ app.use(express.json());
 // Parse incoming JSON requests
 app.use(morgan("dev")); // Log HTTP requests in development mode
 app.get("/", (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Welcome to My E-commerce</title>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin-top: 50px;
-          }
-          h1 {
-            color: #2c3e50;
-          }
-        </style>
-      </head>
-      <body>
-        <h1>Welcome to KFT Foods E-commerce!</h1>
-        <p>This is the home page of your application.</p>
-        <p>Enjoy your stay!</p>
-      </body>
-    </html>
-  `);
+  res.json({
+    host: process.env.Host,
+    port: Number(process.env.port),
+    username: process.env.User_Name,
+    password: process.env.Password,
+    database: process.env.Database,
+  })
+  // res.send(`
+  //   <html>
+  //     <head>
+  //       <title>Welcome to My E-commerce</title>
+  //       <style>
+  //         body {
+  //           font-family: Arial, sans-serif;
+  //           text-align: center;
+  //           margin-top: 50px;
+  //         }
+  //         h1 {
+  //           color: #2c3e50;
+  //         }
+  //       </style>
+  //     </head>
+  //     <body>
+  //       <h1>Welcome to KFT Foods E-commerce!</h1>
+  //       <p>This is the home page of your application.</p>
+  //       <p>Enjoy your stay!</p>
+  //     </body>
+  //   </html>
+  // `);
 });
 // Route Middleware
 app.use(itemsRoute);
